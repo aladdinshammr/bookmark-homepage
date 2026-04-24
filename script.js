001cc29d0a54f51b564c489d588e9877870ca58a
Alaa Aldeen Shammr <91631145+aladdinshammr@users.noreply.github.com>
2026-04-25 01:39:57 +0300

    finish all sections

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 const logo = document.getElementById("logo");
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
 
 function toggleMenu() {
   menuBtn.classList.toggle("open");
@@ -13,4 +15,19 @@ function toggleMenu() {
   }
 }
 
+function togglePanels() {
+  tabs.forEach((tab) =>
+    tab.children[0].classList.remove("border-b-4", "border-softRed"),
+  );
+  this.children[0].classList.add("border-b-4", "border-softRed");
+
+  panels.forEach((panel) => panel.classList.add("hidden"));
+
+  const selectedEl = document.getElementsByClassName(
+    this.getAttribute("data-target"),
+  );
+  selectedEl[0].classList.remove("hidden");
+}
+
 menuBtn.addEventListener("click", toggleMenu);
+tabs.forEach((tab) => tab.addEventListener("click", togglePanels));
